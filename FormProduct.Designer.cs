548b53d5ae57633a3551f890ff2c6aa0f91878e1
ZheerBarzan <zheerbarzan85@outlook.com>
2024-12-06 21:32:44 +0300

    im batman

--- FormProduct.Designer.cs
+++ FormProduct.Designer.cs
@@ -28,19 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            ProductCatagoryLable = new Label();
+            ProdcutName = new Label();
+            ProductPrice = new Label();
             SuspendLayout();
+            // 
+            // ProductCatagoryLable
+            // 
+            ProductCatagoryLable.AutoSize = true;
+            ProductCatagoryLable.Location = new Point(100, 75);
+            ProductCatagoryLable.Name = "ProductCatagoryLable";
+            ProductCatagoryLable.Size = new Size(196, 32);
+            ProductCatagoryLable.TabIndex = 0;
+            ProductCatagoryLable.Text = "Choose Catagory";
+            // 
+            // ProdcutName
+            // 
+            ProdcutName.AutoSize = true;
+            ProdcutName.Location = new Point(100, 174);
+            ProdcutName.Name = "ProdcutName";
+            ProdcutName.Size = new Size(167, 32);
+            ProdcutName.TabIndex = 1;
+            ProdcutName.Text = "Product Name";
+            // 
+            // ProductPrice
+            // 
+            ProductPrice.AutoSize = true;
+            ProductPrice.Location = new Point(100, 265);
+            ProductPrice.Name = "ProductPrice";
+            ProductPrice.Size = new Size(154, 32);
+            ProductPrice.TabIndex = 2;
+            ProductPrice.Text = "Product Price";
             // 
             // FormProduct
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1610, 1157);
+            Controls.Add(ProductPrice);
+            Controls.Add(ProdcutName);
+            Controls.Add(ProductCatagoryLable);
             Name = "FormProduct";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "FormProduct";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label ProductCatagoryLable;
+        private Label ProdcutName;
+        private Label ProductPrice;
     }
 }
--- FormProduct.cs
+++ FormProduct.cs
@@ -16,5 +16,7 @@ namespace PhoneShop
         {
             InitializeComponent();
         }
+
+       
     }
 }
